f5af888be4998370b2776f49f1340fd4dc953435
Samir Dahal <samirdahal877@gmail.com>
2025-11-19 05:23:14 +0545

    indexing to the fields

---
+++ index.py
@@ -0,0 +1,20 @@
+# Create text indexes for filterable fields
+from qdrant_client import models
+
+from qdrant import client, collection_name
+
+
+def make_index(field_name_to_index):
+    for field_name in field_name_to_index:
+        client.create_payload_index(
+            collection_name=collection_name,
+            field_name=field_name,
+            field_schema=models.TextIndexParams(
+                type="text",
+                tokenizer=models.TokenizerType.WORD,
+                phrase_matching=True,
+                min_token_len=2,
+                max_token_len=10,
+                lowercase=True,
+            ),
+        )
