eb02e4cf1b61d1bfcc7b619a715b2ced444c75a3
Samir Dahal <samirdahal877@gmail.com>
2025-11-19 05:24:45 +0545

    main file

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import uvicorn
+
+
 def main():
-    print("Hello from leapfrog-job-search!")
+    uvicorn.run(
+        "api:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+    )
 
 
 if __name__ == "__main__":
