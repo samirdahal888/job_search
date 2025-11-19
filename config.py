6c43c56f6965aef7e1f0a28098e6964711e1b0fe
Samir Dahal <samirdahal877@gmail.com>
2025-11-19 19:24:01 +0545

    added llm,spares,dense modules in config

--- config.py
+++ config.py
@@ -14,8 +14,8 @@ class Settings(BaseSettings):
 
     # Applicaiton settings
     CSV_FILE_PATH: str = Field(
-        default="/home/samir-dahal/leapfrog_job_search/lf_job.csv",
-        description="Path to the csv file (related to project root)",
+        default="lf_job.csv",
+        description="Path to the csv file relative path ",
     )
     COLLECTION_NAME: str = Field(
         default="hybrid_search", description="Qdrant collection name"
@@ -31,7 +31,7 @@ class Settings(BaseSettings):
 
     # API settings
     API_HOST: str = Field(default="0.0.0.0", description="API host")
-    API_port: int = Field(default=8000, description="API port")
+    API_PORT: int = Field(default=8000, description="API port")
     MAX_QUERY_RESULT: int = Field(
         default=20, description="Maximum number of result for a query"
     )
@@ -46,26 +46,37 @@ class Settings(BaseSettings):
     # LLM setting
     LLM_TEMPERATURE: float = Field(default=0.3, description="LLM temperature")
     LLM_MAX_TOKENS: int = Field(default=10000, description="LLM max token output")
+    LLM_MODEL:str =Field(default='gemini-2.5-flash',description="Model used in this project") 
+
+    #sparse model and dense models
+    SPARSE_MODEL:str = Field(default="Qdrant/bm25",description="Model for sparse search")
+    DENSE_MODEL:str =Field(default="sentence-transformers/all-MiniLM-L6-v2",description="Model for dense search")
+
 
     # validating fields contain value
     @field_validator("CSV_FILE_PATH")
+    @classmethod
     def validate_csv_path(cls, v):
         """convert to absoulte path and check if file exists"""
-        csv_path = Path(v)
-        print(csv_path)
+
+        csv_path = Path(__file__).parent / v
         if not csv_path.exists():
             raise ValueError(
                 f" CSV file not found at {csv_path}"
                 f"Please ensure the file exists or update CSV_FILE_PATH in .env"
             )
+        print(str(csv_path.resolve()))
         return str(csv_path.resolve())
 
     @field_validator("QDRANT_API_KEY", "GEMINI_API_KEY")
-    def validate_api_key(cls, v, field):
+    @classmethod
+    def validate_api_key(cls, v, info):
         """Ensures api keys are not empty"""
         if not v or v.strip() == "":
-            raise ValueError(f"{field} is required but found empty")
+            raise ValueError(f"{info.field_name} is required but found empty")
         return v.strip()
 
-
-settings = Settings()
+try:
+    settings = Settings()
+except Exception as e:
+    print(f"Configuration Error:{e}")
