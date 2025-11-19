25cd5196c096b70f174ca3e1990aa7230a3421f4
Samir Dahal <samirdahal877@gmail.com>
2025-11-19 14:47:57 +0545

    added publication date indexing for filtering

--- index.py
+++ index.py
@@ -6,15 +6,27 @@ from qdrant import client, collection_name
 
 def make_index(field_name_to_index):
     for field_name in field_name_to_index:
-        client.create_payload_index(
+        if field_name =='publication_date':
+            client.create_payload_index(
             collection_name=collection_name,
             field_name=field_name,
-            field_schema=models.TextIndexParams(
-                type="text",
-                tokenizer=models.TokenizerType.WORD,
-                phrase_matching=True,
-                min_token_len=2,
-                max_token_len=10,
-                lowercase=True,
+            field_schema=models.DatetimeIndexParams(
+                type=models.DatetimeIndexType.DATETIME,
+                is_principal=True,
             ),
-        )
+)
+      
+        else:
+            # Create text index for other fields
+            client.create_payload_index(
+                collection_name=collection_name,
+                field_name=field_name,
+                field_schema=models.TextIndexParams(
+                    type="text",
+                    tokenizer=models.TokenizerType.WORD,
+                    phrase_matching=True,
+                    min_token_len=2,
+                    max_token_len=10,
+                    lowercase=True,
+                ),
+            )
