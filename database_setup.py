df5f35b52e300ef5d65299468c2e1c5705bb327d
Samir Dahal <samirdahal877@gmail.com>
2025-11-19 14:47:14 +0545

    added publication date in the database

--- database_setup.py
+++ database_setup.py
@@ -1,4 +1,6 @@
 #   database setup
+from datetime import datetime
+
 from clean_data import clean_html
 from create_chunks import create_chunks
 from index import make_index
@@ -16,19 +18,28 @@ for id, row in data.iterrows():
         "location": row.get("Job Location", ""),
         "company": row.get("Company Name", ""),
         "Level": row.get("Job Level", ""),
+        "publication_date": row.get(
+            "Publication Date", ""
+        ),  # Store as datetime object, not string
     }
+    # print(metadata)
 
     chunks = create_chunks(
         job_title=row.get("Job Title"),
         description=row["Job Description"],
         metadata=metadata,
     )
+    # print(chunks)
+    # make_embedding_save_to_db(chunks)
+    # make_index(["category", "location", "company", "Level","publication_date"])
+
+    # break
 
     all_chunks.extend(chunks)
 
 make_embedding_save_to_db(all_chunks)
 
-# making index for the necessary field to make field level search easier and faster
-make_index(["category", "location", "company", "Level"])
+# # making index for the necessary field to make field level search easier and faster
+make_index(["category", "location", "company", "Level","publication_date"])
 
 print("Data base setup completed ")
