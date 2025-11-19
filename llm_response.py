07c97cbd413ed86c78ccf2d0a9404741e7bfee0f
Samir Dahal <samirdahal877@gmail.com>
2025-11-19 19:24:39 +0545

    remove the hardcoded model names

--- llm_response.py
+++ llm_response.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 from config import settings
 
 genai.configure(api_key=settings.GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-2.5-flash")
+model = genai.GenerativeModel(settings.LLM_MODEL)
 
 
 def LLM_response(unique_job_results, original_query):
