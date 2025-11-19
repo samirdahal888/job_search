01de4636ba9666b34ce017303d2f146455abd4eb
Samir Dahal <samirdahal877@gmail.com>
2025-11-19 18:38:33 +0545

    added field_validator to ensure field value and isnot empty

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 
 from pydantic import Field, field_validator
@@ -14,15 +13,15 @@ class Settings(BaseSettings):
     QDRANT_LOCATION: str = Field(..., description="Qdrant cloud location/URL")
 
     # Applicaiton settings
-
     CSV_FILE_PATH: str = Field(
-        default="lf_job.csv",
+        default="/home/samir-dahal/leapfrog_job_search/lf_job.csv",
         description="Path to the csv file (related to project root)",
     )
-    COLLECTION_NAME: str =Field(
-        default="hybrid_search",
-        description="Qdrant collection name"
+    COLLECTION_NAME: str = Field(
+        default="hybrid_search", description="Qdrant collection name"
     )
+
+    # loading the .env files
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
@@ -31,21 +30,42 @@ class Settings(BaseSettings):
     )
 
     # API settings
-    API_HOST:str = Field(default='0.0.0.0',description="API host")
-    API_port:int = Field(default=8000,description="API port")
-    MAX_QUERY_RESULT: int = Field(default=20,description="Maximum number of result for a query")
-    DEFAULT_QUERY_RESULT: int =Field(default=3,description="Default number of result per query")
+    API_HOST: str = Field(default="0.0.0.0", description="API host")
+    API_port: int = Field(default=8000, description="API port")
+    MAX_QUERY_RESULT: int = Field(
+        default=20, description="Maximum number of result for a query"
+    )
+    DEFAULT_QUERY_RESULT: int = Field(
+        default=3, description="Default number of result per query"
+    )
 
-    # chunks related  settings 
+    # chunks related  settings
+    CHUNK_SIZE: int = Field(default=300, description="Text chunk size for splitting")
+    CHUNK_OVERLAP: int = Field(default=30, description="overlap between chunks")
 
-    CHUNK_SIZE:int =Field(default=300,description='Text chunk size for splitting')
-    CHUNK_OVERLAP: int = Field(default=30,description="overlap between chunks")
+    # LLM setting
+    LLM_TEMPERATURE: float = Field(default=0.3, description="LLM temperature")
+    LLM_MAX_TOKENS: int = Field(default=10000, description="LLM max token output")
 
-    # LLM setting 
-    LLM_TEMPERATURE: float = Field(default=0.3,description="LLM temperature")
-    LLM_MAX_TOKENS:int = Field(default=10000,description="LLM max token output")
+    # validating fields contain value
+    @field_validator("CSV_FILE_PATH")
+    def validate_csv_path(cls, v):
+        """convert to absoulte path and check if file exists"""
+        csv_path = Path(v)
+        print(csv_path)
+        if not csv_path.exists():
+            raise ValueError(
+                f" CSV file not found at {csv_path}"
+                f"Please ensure the file exists or update CSV_FILE_PATH in .env"
+            )
+        return str(csv_path.resolve())
 
-    
+    @field_validator("QDRANT_API_KEY", "GEMINI_API_KEY")
+    def validate_api_key(cls, v, field):
+        """Ensures api keys are not empty"""
+        if not v or v.strip() == "":
+            raise ValueError(f"{field} is required but found empty")
+        return v.strip()
 
 
 settings = Settings()
