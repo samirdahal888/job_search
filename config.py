ac419dfa23d679cdaa5ed626ce66bcc3bc94de6a
Samir Dahal <samirdahal877@gmail.com>
2025-11-19 17:05:34 +0545

    added settingsconfigdict to read the .env file.

--- config.py
+++ config.py
@@ -1,23 +1,30 @@
 import os
 from pathlib import Path
+
+from pydantic import Field, field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import Field,field_validator
+
 
 class Settings(BaseSettings):
     """Application settings with validation"""
+
     # .env file validations
-    GEMINI_API_KEY: str=Field(...,description="Google Gemini API key")
-    QDRANT_API_KEY: str = Field(...,description="Qdrat cloud API key")
-    QDRANT_LOCATION:str = Field(...,description="Qdrant cloud location/URL")
+    GEMINI_API_KEY: str = Field(..., description="Google Gemini API key")
+    QDRANT_API_KEY: str = Field(..., description="Qdrat cloud API key")
+    QDRANT_LOCATION: str = Field(..., description="Qdrant cloud location/URL")
 
-    #Applicaiton settings 
+    # Applicaiton settings
 
-    CSV_FILE_PATH: str =Field(
-        default='lf_job.csv',
-        description="Path to the csv file (related to project root)"
-
+    CSV_FILE_PATH: str = Field(
+        default="lf_job.csv",
+        description="Path to the csv file (related to project root)",
+    )
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=True,
+        extra="ignore",
     )
 
-settings  =Settings()
 
-
+settings = Settings()
