7531045271ed88ebc24c465015c7afd301dbd6cc
Samir Dahal <samirdahal877@gmail.com>
2025-11-22 13:39:28 +0545

    search module config

---
+++ search/config.py
@@ -0,0 +1,9 @@
+"""Search module configuration"""
+
+from common.base_config import BaseConfig
+
+
+class SearchConfig(BaseConfig):
+    """Configuration for search module - inherits all from base"""
+
+    pass
