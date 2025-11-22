d53b2174d8acd73e86e0c5960f997016f925b109
Samir Dahal <samirdahal877@gmail.com>
2025-11-22 13:17:37 +0545

    cleaning up dead code

--- clean_data.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-
-
-def remove_html_tags(description):
-    soup = BeautifulSoup(description, "html.parser")
-    text = soup.get_text(separator=" ")
-
-    return text
--- load_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-from config import settings
-
-
-def load_data():
-    df = pd.read_csv(settings.CSV_FILE_PATH)
-    return df
