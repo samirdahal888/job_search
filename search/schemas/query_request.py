23b06bebccf796323c66d5672ce56dd2043f031e
Samir Dahal <samirdahal877@gmail.com>
2025-11-25 17:24:34 +0545

    added a default value in query

--- search/schemas/query_request.py
+++ search/schemas/query_request.py
@@ -6,7 +6,7 @@ from api_config import api_config
 class QueryRequest(BaseModel):
     """Request model for job search query"""
 
-    query: str = Field(max_length=120, min_length=2)
+    query: str = Field(default="I need Job",max_length=120, min_length=2)
     top: int = Field(
         default=api_config.DEFAULT_QUERY_RESULT, ge=1, le=api_config.MAX_QUERY_RESULT
     )
